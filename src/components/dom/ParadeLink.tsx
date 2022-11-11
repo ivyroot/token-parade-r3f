cfa3deffcdd260dd72555e95440a99447e16e8ba
ivyroot <nonagoncup@gmail.com>
2022-11-11 13:42:04 -0800

    add link to wallet to UI

---
+++ src/components/dom/ParadeLink.tsx
@@ -0,0 +1,20 @@
+// tsx div showing external link
+// Path: src/components/dom/ParadeLink.tsx
+// Compare this snippet from src/components/dom/AddressInput.tsx:
+//             <div className='flex flex-row justify-between'>
+//               <div className='flex flex-row'>
+export const ParadeLink = (props) => {
+    const link = props.a ? `https://www.tokenparade.xyz/?a=${props.a}` : ''
+    const linkText = props.a ? `www.tokenparade.xyz/?a=${props.a}` : ''
+    if (link === '') {
+        return null
+    }
+    return(
+        <a href={link} target='_blank' className='block px-1 mx-8 my-2'>
+            <div className='flex'>
+                <img src='/ic-external-link.svg' alt='external link' className="w-2.5 pt-0.5 mr-1.5" />
+                <div className='text-slate-600'>{linkText}</div>
+            </div>
+        </a>
+    )
+}
