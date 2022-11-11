90e2e13cea0d889685fe85102be67fbefd7b5d4b
ivyroot <nonagoncup@gmail.com>
2022-11-11 13:47:42 -0800

    update to external url per recs

--- src/components/dom/ParadeLink.tsx
+++ src/components/dom/ParadeLink.tsx
@@ -5,7 +5,7 @@ export const ParadeLink = (props) => {
         return null
     }
     return(
-        <a href={link} target='_blank' className='block px-1 mx-8 my-2'>
+        <a href={link} target='_blank' rel="noreferrer" className='block px-1 mx-8 my-2'>
             <div className='flex'>
                 <img src='/ic-external-link.svg' alt='external link' className="w-2.5 pt-0.5 mr-1.5" />
                 <div className='text-slate-600'>{linkText}</div>
